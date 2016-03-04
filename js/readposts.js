07d6f06dd1bc8b71f0739b9622de2a10ae391992
Christopher Saunders <cjsaunders22@gmail.com>
2016-03-04 01:06:10 -0500

    Add collection pages and more resources

---
+++ js/readposts.js
@@ -0,0 +1,40 @@
+function GetPosts(){
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	}
+	else{
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	xmlhttp.open("GET", "../res/news.xml", false);
+	xmlhttp.send();
+
+	xmlDoc = xmlhttp.responseXML;
+	
+	rootElement = xmlDoc.getElementsByTagName("posts")[0];
+	posts = rootElement.getElementsByTagName("post");
+	
+	for(i = 0; i < posts.length; i++){
+		title = posts[i].getAttribute("title");
+		date = posts[i].getAttribute("date");
+		text = posts[i].childNodes[0].nodeValue;
+		
+		newPost = document.createElement("div");
+			newPost.className = "post"
+		newTitle = document.createElement("h3");
+		newText = document.createElement("p");
+		
+		titleText = document.createTextNode(title);
+		dateText = document.createTextNode(date);
+		newTitle.appendChild(titleText);
+		
+		postText = document.createTextNode(text);
+		newText.appendChild(postText);
+		
+		newPost.appendChild(newTitle);
+		newPost.appendChild(newText);
+		
+		content = document.getElementById("content");
+		content.appendChild(newPost);
+	}
+}

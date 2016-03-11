a101825c44f491bb7614f5c3526d594a451ff9fe
Christopher Saunders <cjsaunders22@gmail.com>
2016-03-11 11:48:05 -0500

    Add more styling adjustments

--- js/readposts.js
+++ js/readposts.js
@@ -1,8 +1,8 @@
-function GetPosts(){
-	if(window.XMLHttpRequest){
+function GetPosts() {
+	if (window.XMLHttpRequest) {
 		xmlhttp = new XMLHttpRequest();
 	}
-	else{
+	else {
 		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	
@@ -14,7 +14,7 @@ function GetPosts(){
 	rootElement = xmlDoc.getElementsByTagName("posts")[0];
 	posts = rootElement.getElementsByTagName("post");
 	
-	for(i = 0; i < posts.length; i++){
+	for(i = 0; i < posts.length; i++) {
 		title = posts[i].getAttribute("title");
 		date = posts[i].getAttribute("date");
 		text = posts[i].childNodes[0].nodeValue;
